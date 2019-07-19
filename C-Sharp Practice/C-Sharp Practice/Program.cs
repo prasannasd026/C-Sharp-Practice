39f4c58e2c73aa47b20bbf141921480293a1fb7f
Prasanna Deshmukh <prasannadeshmukh@hotmail.com>
2019-07-19 16:35:48 +0530

    Static Methods/Classes tutorial

--- C-Sharp Practice/C-Sharp Practice/Program.cs
+++ C-Sharp Practice/C-Sharp Practice/Program.cs
@@ -6,7 +6,35 @@ namespace C_Sharp_Practice
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            car myCar = new car();
+            myCar.Color = "Blue";
+
+            car myCar2 = new car();
+            myCar2.Color = "Red";
+
+            Console.WriteLine(myCar.Color);
+            Console.WriteLine(myCar2.Color);
+
+            int total = Math.Add(5, 3);
+            int total2 = Math.Add(6, 10);
+
+            Console.WriteLine(total);
+            Console.WriteLine(total2);
+
+            Console.ReadLine();
+        }
+    }
+
+    class car
+    {
+        public string Color { get; set; }
+    }
+
+    static class Math
+    {
+        public static int Add(int a, int b)
+        {
+            return a + b;
         }
     }
 }
